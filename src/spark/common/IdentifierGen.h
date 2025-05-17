1d60c5a63ebe9f8d6a338b855fd3ca67ce470ff4
subreax <dima2000947@gmail.com>
2025-05-17 21:27:08 +0300

    Improve IdentifierGen

--- src/spark/common/IdentifierGen.h
+++ src/spark/common/IdentifierGen.h
@@ -6,18 +6,7 @@ public:
     IdentifierGen(LinearAllocator& allocator) : allocator(allocator) { }
 
     const char* unique(const char* id) {
-        int nameLen = strlen(id);
-        if (nameLen >= MAX_ID_LEN) {
-            return ".err_name_too_long";
-        }
-
-        int len = sprintf(nameBuf, "%s.%d", id, counter);
-        if (len < 0) {
-            return ".err_failed_to_build";
-        }
-
-        counter++;
-
+        auto len = generate(id);
         auto allocatedStr = (char*) allocator.allocate(len + 1);
         if (allocatedStr == nullptr) {
             return ".err_out_of_mem";
@@ -27,10 +16,27 @@ public:
         return allocatedStr;
     }
 
-    static constexpr int MAX_ID_LEN = 28;
+    static constexpr size_t MAX_ID_LEN = 16;
+    static constexpr const char* ERR_NO_MEM = ".err_out_of_mem";
 
 private:
-    char nameBuf[40];
+    size_t generate(const char* id) {
+        size_t len = copy(nameBuf, id, MAX_ID_LEN);
+        len += sprintf(nameBuf + len, ".%d", counter);
+        counter++;
+        return len;
+    }
+
+    static size_t copy(char* to, const char* from, size_t max) {
+        size_t i = 0;
+        for ( ; *from && i < max; i++) {
+            to[i] = *from;
+            from++;
+        }
+        return i;
+    }
+
+    char nameBuf[MAX_ID_LEN + 16];
     LinearAllocator& allocator;
     int counter = 0;
 };
